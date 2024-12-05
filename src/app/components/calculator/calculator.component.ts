80a99d5177f41d9f8a288b936f3bb7c4a24a2919
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-12-05 18:50:37 +0200

    Реализовал базовую логику конвертации

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,22 +1,61 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {map, Observable} from 'rxjs';
 import {CurrencyService} from '../../services/currency.service';
+import {ResponseExchangeInterface} from '../../types/response-exchange.interface';
+import {SUPPORTED_CURRENCIES} from '../../mock/supported-currancies';
+import {FormsModule} from '@angular/forms';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf,
+    AsyncPipe,
+    NgForOf
+  ],
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
 
-  exchangeRates$!: Observable<any>;
+  exchangeRates$!: Observable<{ [key: string]: number }>;
   currencyService = inject(CurrencyService);
 
+  amount: number = 0;
+  fromCurrency: string = 'UAH';
+  toCurrency: string = 'USD';
+  result: number | null = null;
+  supportedCurrencies = SUPPORTED_CURRENCIES;
+
   ngOnInit() {
-    this.currencyService.getExchangeRates().subscribe({
-      next: data => console.log(data),
-      error: error => console.log(error),
-    });
+    // Получаем курсы валют и преобразуем их в объект с парами "код валюты: курс"
+    this.exchangeRates$ = this.currencyService.getExchangeRates().pipe(
+      map((data: ResponseExchangeInterface[]) => {
+        return data.reduce((acc: { [key: string]: number }, rate: ResponseExchangeInterface) => {
+          if (SUPPORTED_CURRENCIES.find(currency => currency.code === rate.cc)) {
+            acc[rate.cc] = rate.rate;
+          }
+          return acc;
+        }, {});
+      })
+    );
+  }
+
+  convert(exchangeRates: { [key: string]: number }) {
+    if (this.fromCurrency === this.toCurrency) {
+      // Если валюты одинаковые, сумма остается неизменной
+      this.result = this.amount;
+    } else if (this.fromCurrency === 'UAH') {
+      // Конвертация из гривны в другую валюту
+      this.result = this.amount / exchangeRates[this.toCurrency];
+    } else if (this.toCurrency === 'UAH') {
+      // Конвертация из другой валюты в гривну
+      this.result = this.amount * exchangeRates[this.fromCurrency];
+    } else {
+      // Конвертация между двумя иностранными валютами через гривну
+      const amountInUah = this.amount * exchangeRates[this.fromCurrency];
+      this.result = amountInUah / exchangeRates[this.toCurrency];
+    }
   }
 }
---
+++ src/app/mock/supported-currancies.ts
@@ -0,0 +1,11 @@
+export const SUPPORTED_CURRENCIES: { code: string, name: string }[] = [
+  {code: 'USD', name: 'Доллар США'},
+  {code: 'EUR', name: 'Евро'},
+  {code: 'UAH', name: 'Украинская гривна'},
+  {code: 'PLN', name: 'Польский злотый'},
+  {code: 'GBP', name: 'Британский фунт'},
+  {code: 'CHF', name: 'Швейцарский франк'},
+  {code: 'CZK', name: 'Чешская крона'},
+  {code: 'SEK', name: 'Шведская крона'}
+];
+
