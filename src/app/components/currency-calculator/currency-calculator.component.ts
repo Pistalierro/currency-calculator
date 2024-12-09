1646cf8c2e7f6e7da04b00529ec5fe79bc8a1118
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-12-09 17:54:02 +0200

    Подготовил проект к работе

---
+++ src/app/components/currency-calculator/currency-calculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './currency-calculator.component.html',
+  styleUrl: './currency-calculator.component.scss'
+})
+export class CurrencyCalculatorComponent {
+
+}
